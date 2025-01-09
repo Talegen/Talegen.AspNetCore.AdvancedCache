e3dc5f539e786cfef79b307f7453d8768faaa824
Rob Kennedy <rob@talegen.com>
2025-01-09 14:10:32 -0500

    implement changes in service registration allowing for keyed services. This allows developers to define seperate keyed instances of the multiplexor for different connection strings (e.g. specifying a different regEx database per connection string)

--- src/Redis/RedisClientServiceExtensions.cs
+++ src/Redis/RedisClientServiceExtensions.cs
@@ -18,6 +18,7 @@ namespace Talegen.AspNetCore.AdvancedCache.Redis
     using System;
     using Microsoft.Extensions.Caching.Distributed;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
 
     /// <summary>
     /// This class is used for adding Distributed Cache according to configuration
@@ -29,8 +30,10 @@ namespace Talegen.AspNetCore.AdvancedCache.Redis
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
         /// <param name="setupAction">An <see cref="Action{RedisCacheOptions}" /> to configure the provided <see cref="RedisCacheOptions" />.</param>
+        /// <param name="keyedServiceId">An optional keyed service identifier.</param>
+        /// <param name="registerIDistributedCache">A value indicating whether to register the advanced service as an override of <see cref="IDistributedCache" /> service.</param>
         /// <returns>The <see cref="IServiceCollection" /> so that additional calls can be chained.</returns>
-        public static IServiceCollection AddRedisClientCache(this IServiceCollection services, Action<RedisClientCacheOptions> setupAction)
+        public static IServiceCollection AddRedisClientCache(this IServiceCollection services, Action<RedisClientCacheOptions> setupAction, string keyedServiceId = "", bool registerIDistributedCache = false)
         {
             if (services == null)
             {
@@ -44,11 +47,26 @@ namespace Talegen.AspNetCore.AdvancedCache.Redis
 
             services.AddOptions();
             services.Configure(setupAction);
-            services.AddSingleton<IAdvancedDistributedCache, AdvancedRedisCache>();
-            services.AddSingleton<IDistributedCache>(r =>
+
+            // If the keyed service id is empty, then we are using the default configuration.
+            if (string.IsNullOrWhiteSpace(keyedServiceId))
             {
-                return r.GetRequiredService<IAdvancedDistributedCache>();
-            });
+                services.TryAddSingleton<IAdvancedDistributedCache, AdvancedRedisCache>();
+
+                if (registerIDistributedCache)
+                {
+                    services.TryAddSingleton<IDistributedCache>(r =>
+                    {
+                        return r.GetRequiredService<IAdvancedDistributedCache>();
+                    });
+                }
+            }
+            else
+            {
+                // If the keyed service id is not empty, then we are using a named configuration.
+                services.TryAddKeyedSingleton<IAdvancedDistributedCache, AdvancedRedisCache>(keyedServiceId);
+            }
+
             return services;
         }
     }
